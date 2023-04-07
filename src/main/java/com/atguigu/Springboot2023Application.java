6644fdb46fd13134e34b7fba539aeccb74c31bb9
张世罡 <1724473740@qq.com>
2023-04-07 16:26:24 +0800

    INIT

---
+++ src/main/java/com/atguigu/Springboot2023Application.java
@@ -0,0 +1,13 @@
+package com.atguigu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2023Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot2023Application.class, args);
+    }
+
+}
